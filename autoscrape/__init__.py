d6263872596a9fcb952e119c932786e350f74ef1
Brandon Roberts <brandon@bxroberts.org>
2018-05-17 21:03:22 -0700

    overall control flow loop

--- autoscrape/__init__.py
+++ autoscrape/__init__.py
@@ -1,32 +1,42 @@
 """
-                   given (initial page), load it
+                   initialize & get entry point
                                │
                                ↓
-              look for search form (possibly classifier)
-                               │
+                           load page    🠤───────────────────┐
+                               │                            │
+                               │            click a link based on likelihood
+                               │               of finding a search form
+                               ↓                            │
+       ┌────🠦 look for search form (possibly classifier) ───┘
+       │                       │
+       │                       │ FOUND
+       │                       ↓
+       │         identify forms on page that require input
+       │     (begin with config then move to heuristic then ML)
+       │                       │
+       │                       ↓
+       │      initialize iterators for required inputs
+       │      (begin with config/brute force, then RL)
+       │                       │
+       │                       ↓
+       └─────── are we at the end of our iterators?
+          YES                  │
                                ↓
-               identify forms on page that require input
-           (begin with config then move to heuristic then ML)
-                               │
-                               ↓
-              initialize iterators for required inputs
-              (begin with config/brute force, then RL)
-                               │
-                               ↓
-                   enter data into form inputs
-                               │
-                               ↓
-                  submit form and load next page
-                               │
-                               ↓
-           ┌──────────🠦 scrape the page
-           │                   │
-           │                   ↓
-           │         look for a next button
-           │             (classifier)
-           │                   │
-           │                   ↓ YES
-           └─────── click the next button & load page
+                   enter data into form inputs 🠤───────┐
+                               │                       │
+                               ↓                       │
+                  submit form and load next page       │
+                               │                       │
+                               ↓                       │
+               ┌──────🠦 scrape the page                │
+               │               │                       │
+               │               ↓                       │
+               │     look for a next button ───────────┘
+               │         (classifier)        NOT FOUND
+               │               │
+               │               │ YES
+               │               ↓
+               └─── click the next button & load page
 """
 class AutoScraper(object):
     pass
