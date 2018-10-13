1612d37f8d3a0e43fd4d1f8bc2eb870406aba6e7
Brandon Roberts <brandon@bxroberts.org>
2018-10-13 15:14:55 -0700

    Bugfix: include the graph classes

---
+++ autoscrape/search/bfs.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+from collections import deque
+
+import networkx as nx
+
+
+class BFS(object):
+    def __init__(self, root_node):
+        # queue of remaining BFS nodes
+        self.queue = deque()
+        # store BFS scrape graph
+        self.graph = nx.DiGraph()
+        # current node (css tag)
+        self.current = None
+
+    def next(self):
+        """
+        Get the next CSS path node to interact with.
+        """
+        newcurrent = self.queue.popleft()
+        self.current = newcurrent
+        return newcurrent
+
+    def add_root_node(self, node, **kwargs):
+        """
+        Add an isolated node, make it the current node. This
+        is for creating new subgraphs inside our graph. Does
+        not add any edges.
+        """
+        self.graph.add_node(node, **kwargs)
+        self.current = node
+
+    def add_node(self, node, **kwargs):
+        """
+        Add a single node to the tree, with edges connecting
+        to the current node.
+        """
+        self.queue.append(nodes)
+        self.graph.add_node(node, **kwargs)
+        self.graph.add_edge(self.current, node)
+
+    def add_nodes(self, nodes):
+        """
+        Add a list of nodes to the current node in the graph.
+        This handles adding the nodes and the edges.
+        """
+        self.queue.extend(nodes)
+        for node, meta in nodes:
+            self.add_node(node, **meta)
+
---
+++ autoscrape/search/graph.py
@@ -0,0 +1,72 @@
+# -*- coding: UTF-8 -*-
+from collections import deque
+import logging
+
+import networkx as nx
+
+
+logger = logging.getLogger('AUTOSCRAPE')
+
+
+class Graph(object):
+    def __init__(self):
+        # store scrape graph
+        self.graph = nx.DiGraph()
+        # current node (css tag)
+        self.current = None
+        self.dbg("Initializing graph")
+
+    def dbg(self, msg):
+        logger.debug("[GRAPH]%s", msg)
+
+    def add_root_node(self, node, **kwargs):
+        """
+        Add an isolated node, make it the current node. This
+        is for creating new subgraphs inside our graph. Does
+        not add any edges.
+        """
+        self.dbg("Adding root node as current: %s" % node)
+        self.graph.add_node(node, **kwargs)
+        self.current = node
+
+    def add_node(self, node, **kwargs):
+        """
+        Add a single node to the tree, with edges connecting
+        to the current node.
+        """
+        self.dbg("Adding to current: %s, Node: %s, Meta: %s" % (
+            self.current, node, kwargs))
+        self.graph.add_node(node, **kwargs)
+        self.graph.add_edge(self.current, node)
+
+    def add_nodes(self, nodes):
+        """
+        Add a list of nodes to the current node in the graph.
+        This handles adding the nodes and the edges.
+        """
+        self.dbg("Adding to current: %s, Nodes: %s" % (self.current, nodes))
+        for node, meta in nodes:
+            self.add_node(node, **meta)
+
+    def add_meta_to_current(self, **meta):
+        self.dbg("Adding meta to current: %s, Meta: %s" % (self.current, meta))
+        self.graph.nodes[self.current].update(**meta)
+
+    def move_to_node(self, node):
+        self.dbg("Moving to node: %s" % node)
+        self.current = node
+
+    def move_to_parent(self):
+        self.dbg("Moving to parent from current: %s" % self.current)
+        try:
+            preds = self.graph.predecessors(self.current)
+            [ self.dbg("Parent: %s" % p) for p in  preds ]
+            parent = self.graph.predecessors(self.current).__next__()
+        except StopIteration:
+            self.dbg("Stop iteration exception hit! Current: %s" % self.current)
+            return
+        self.move_to_node(parent)
+
+    def save_graph(self, output_path):
+        nx.write_gpickle(self.graph, output_path)
+
