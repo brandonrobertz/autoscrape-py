d3255c4607e85d25aaf41d0ffbca8ad7ea1839a7
Brandon Roberts <brandon@bxroberts.org>
2018-10-14 16:08:16 -0700

    Use a standard user-agent for direct downloads

--- autoscrape/web.py
+++ autoscrape/web.py
@@ -520,7 +520,16 @@ class Scraper(object):
         selenium driver due to CORS issues.
         """
         logger.info("Fetching non-HTML page directly: %s" % url)
-        response = urllib.request.urlopen(url)
+        user_agent = (
+            "Mozilla/5.0 "
+            "(Windows NT 10.0; Win64; x64; rv:62.0) "
+            "Gecko/20100101 Firefox/62.0"
+        )
+        request = urllib.request.Request(url, headers={
+            "User-Agent": user_agent,
+            "Referrer": self.page_url,
+        })
+        response = urllib.request.urlopen(request)
         data = response.read()
         action = {
             "action": "download_page",
