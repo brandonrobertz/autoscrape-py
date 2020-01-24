59aa1dff55056f5e5135a86ba65aa13841818fbe
Brandon Roberts <brandon@bxroberts.org>
2020-01-24 12:34:21 -0800

    Parallelize stylesheet downloading

--- autoscrape/backends/requests/dom.py
+++ autoscrape/backends/requests/dom.py
@@ -1,9 +1,19 @@
 # -*- coding: UTF-8 -*-
 import lxml.html
+from multiprocessing import Pool
 
+import requests
 from urllib.parse import urlparse, ParseResult
 
 from autoscrape.backends.base.dom import DomBase
+
+
+def download_stylesheet(css_url):
+    response = requests.get(css_url)
+    data = response.text
+    if type(data) == bytes:
+        return text.decode("utf-8")
+    return data
 
 
 class Dom(DomBase):
@@ -14,10 +24,10 @@ class Dom(DomBase):
     def _get_dom(self):
         return lxml.html.fromstring(self.current_html)
 
-    def element_attr(self, element, name):
-        if not element.attrib:
-            return None
-        return element.attrib.get("href")
+    def element_attr(self, element, name, default=None):
+        if element.attrib and "href" in element.attrib:
+            return element.attrib.get("href")
+        return default
 
     def element_by_tag(self, tag):
         return self.dom.cssselect(tag)[0]
@@ -25,9 +35,9 @@ class Dom(DomBase):
     def elements_by_path(self, path):
         return self.dom.xpath(path)
 
-    def get_stylesheet(self):
+    def get_stylesheet(self, fetch_css=False):
         # TODO: parallelize this
-        css = ""
+        stylesheet_urls = []
         for link in self.dom.xpath("//link"):
             if not link.attrib:
                 continue
@@ -37,9 +47,13 @@ class Dom(DomBase):
             if l_type != "text/css" and l_rel != "stylesheet":
                 continue
             css_url = self.normalize(l_href)
-            stylesheet = self.download_file(css_url, return_data=True)
-            css += stylesheet.decode("utf-8")
-            # css += '@import url("%s");' % (l_href)
+            stylesheet_urls.append(css_url)
+
+        pool = Pool(8)
+        results = pool.map(download_stylesheet, stylesheet_urls)
+        pool.close()
+
+        css = "\n".join(results)
         for style in self.dom.xpath("style"):
             css += style.text_content()
         return css
