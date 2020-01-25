439d060c2acb804c3db685547699ce42c3b34570
Brandon Roberts <brandon@bxroberts.org>
2020-01-25 00:36:35 -0800

    Always use local script for tests

--- tests/common.sh
+++ tests/common.sh
@@ -1,15 +1,9 @@
 BENCHMARK_LOG=backend-benchmark.log
 BACKENDS="selenium requests"
 TIME="$(which time) -p -a -o ${BENCHMARK_LOG}"
-#SCRIPT="autoscrape"
-#if ! which autoscrape 2> /dev/null > /dev/null; then
-#  SCRIPT="./autoscrape.py"
-#fi
-
- SCRIPT="./autoscrape.py"
 echo "Invoking AutoScrape via ${SCRIPT}"
 
-AUTOSCRAPE="${SCRIPT} --save-graph --loglevel DEBUG"
+AUTOSCRAPE="./autoscrape.py --save-graph --loglevel DEBUG"
 
 die () {
   echo "${*}"
