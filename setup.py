008d2deef0b9a51f271bfc99b7bf80d72eb557e5
Brandon Roberts <brandon@bxroberts.org>
2020-02-11 12:50:07 -0800

    v1.1.8

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_long_description():
 
 setuptools.setup(
     name='autoscrape',
-    version='1.1.7',
+    version='1.1.8',
     description='An automated, programming-free web scraper for interactive sites',
     long_description=get_long_description(),
     author='Brandon Roberts',
